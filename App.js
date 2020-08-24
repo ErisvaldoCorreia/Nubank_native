f45fa64543a667d9996500615ebc10a3ecb51941
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-08-24 09:08:36 -0300

    criando paginação e estilos

--- App.js
+++ App.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Main from './src/Main';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>
-        Olá Mundo React Native
-      </Text>
-      <Text style={styles.texto}>
-        Eu serei um estudo clone!
-      </Text>
-      <Text>
-        Nubank Interface
-      </Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
+      <Main />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  texto: {
-    color: '#cc5ffa',
-    fontSize: 20,
-  }
-});
---
+++ src/Main/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    background: #8b10ae;
+    flex: 1;
+`;
