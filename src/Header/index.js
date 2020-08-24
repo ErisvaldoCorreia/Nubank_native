262af186bcda11701d63caecbc07b506dd23adf6
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-08-24 10:13:09 -0300

    ajustes no header e inclusão no main

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Container, Top, Logo, Title } from './Headerstyles';
+import { Container, Top, Logo, Title } from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import logo from '../assets/Nubank_Logo.png';
+import logo from '../../assets/Nubank_Logo.png';
 
 export default function Header() {
   return (
